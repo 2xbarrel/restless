5f2b011686d1cbc58533d3bca578a1cdbece18a9
2xbarrel <barrelthegreat@gmail.com>
2023-04-16 09:25:03 +0300

    testnovel2

--- noveltest/novel.js
+++ noveltest/novel.js
@@ -1,7 +1,8 @@
 let dialogueNum = 0;
 let char1 = document.getElementById("char1");
 let char2 = document.getElementById("char2");
-let emotion = "IDLE";
+let emotion1 = "IDLE";
+let emotion2 = "IDLE";
 
 const visual = document.getElementById("visual");
 const dialogue = document.getElementById("dialogue");
@@ -28,6 +29,11 @@ const appear = [
     { opacity: 1 },
 ];
 
+const disAppear = [
+    { opacity: 1 },
+    { opacity: 0 },
+];
+
 const appearRight = [
     { transform: "translateX(0px)" },
     { transform: "translateX(200px)" },
@@ -48,32 +54,54 @@ const appearTiming = {
 
 button.addEventListener("click", async () => {
 
-    char1.setAttribute("src", `char1${emotion}.png`);
-    char2.setAttribute("src", `char1${emotion}.png`);
+    char1.setAttribute("src", `char1${emotion1}.png`);
+    char2.setAttribute("src", `char2${emotion2}.png`);
 
     dialogueNum += 1;
 
     switch (dialogueNum) {
         case 1:
-            emotion = "HAPPY";
+            emotion1 = "HAPPY";
+            emotion2 = "IDLE";
             char1.animate(appear, appearTiming);
-            dialogue.innerHTML = `Barrel:
+            dialogue.innerHTML = `CharName:
             <p>This is a test NUMBER 2 bla bla bla bla bla bla bla bla</p>`
             break;
         case 2:
-            emotion = "ANGRY";
+            emotion1 = "ANGRY";
+            emotion2 = "HAPPY";
             char2.animate(appearRight, appearTiming);
-            dialogue.innerHTML = `Barrel:
+            dialogue.innerHTML = `CharName:
             <p>This is a test NUMBER 3 bla bla bla bla bla bla bla bla</p>`
             break;
         case 3:
-            emotion = "IDLE"
-            char1.animate(tilt, timing);
-            dialogue.innerHTML = `Barrel:
+            emotion1 = "IDLE"
+            emotion2 = "ANGRY";
+            char1.animate(shake, timing);
+            dialogue.innerHTML = `CharName:
             <p>This is a test NUMBER 4 bla bla bla bla bla bla bla bla</p>`
             break;
         case 4: 
-            window.location.href = "../index.html";
+            emotion1 = "ANGRY";
+            emotion2 = "IDLE";
+            char2.animate(shake, timing);
+            dialogue.innerHTML = `CharName:
+            <p>This is a test NUMBER 5 bla bla bla bla bla bla bla bla</p>`
+            break;
+        case 5: 
+            char1.animate(shake, timing);
+            char2.animate(shake, timing);
+            emotion1 = "HAPPY";
+            emotion2 = "HAPPY";
+            dialogue.innerHTML = `CharName:
+            <p>This is a test NUMBER 6 bla bla bla bla bla bla bla bla</p>`
+            break;
+        case 6: 
+            char1.animate(disAppear, appearTiming);
+            char2.animate(disAppear, appearTiming);
+            dialogue.innerHTML = `CharName:
+            <p>This is a test NUMBER 7 bla bla bla bla bla bla bla bla</p>`
+            dialogueNum = 0;
             break;
     };
     
